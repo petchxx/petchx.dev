59c30ef599e237da86bc3755ac828393621929c2
Petchx <petchxpanuphong@gmail.com>
2024-02-02 10:10:20 +0700

    featL add contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Nav from '@/components/Nav'
+import ContactPage from '@/components/ContactPage'
 
 export default function Contact() {
     return (
         <main>
             <Nav index='contact' />
-
+            <ContactPage />
         </main>
     )
 }
---
+++ components/ContactPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+export default function ContactPage({ }: Props) {
+    return (
+        <main className=''>
+            <div className='max-w-7xl h-10 flex justify-between mx-auto px-6'>
+                Contact me comming soon
+            </div>
+        </main>
+    )
+}
