3d8213978dad520efee1c81ef7d0cf08b6300014
Petchx <petchxpanuphong@gmail.com>
2024-04-27 15:44:01 +0700

    feat: add analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import Footer from "../components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={outfit.className}>
         <Providers>
           {children}
+          <Analytics />
           <Footer />
         </Providers>
       </body>
